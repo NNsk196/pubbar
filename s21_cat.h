96c65e1c86be8f4ce0580a6e3db7d542af340b87
NNsk196 <138286059+NNsk196@users.noreply.github.com>
2025-01-12 19:05:51 +0300

    Create s21_cat.h

---
+++ s21_cat.h
@@ -0,0 +1,18 @@
+#ifndef FLAGS_H
+#define FLAGS_H
+
+typedef struct {
+  int flagS;
+  int flagB;
+  int flagN;
+  int flagE;
+  int flagV;
+  int flagT;
+} Flags;
+
+typedef struct {
+  Flags *flags;
+  char **filename;
+} opt_t;
+
+#endif
